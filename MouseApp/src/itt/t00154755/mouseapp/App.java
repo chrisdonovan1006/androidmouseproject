a8b2fa14da50dde6ba1fad90f94ab24a4bbbae81
chrisdonovan1006 <chrisd25@live.ie>
2013-03-17 12:38:07 +0000

    Starting a new Repo

---
+++ MouseApp/src/itt/t00154755/mouseapp/App.java
@@ -0,0 +1,23 @@
+package itt.t00154755.mouseapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class App extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.app, menu);
+        return true;
+    }
+    
+}
