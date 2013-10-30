2d1e775945bda22faa1f5fc94cdb8db9f177078d
chrisdonovan1006 <chrisd25@live.ie>
2013-10-30 12:39:31 +0000

    Removed Old and Un-used Classes and Folders

--- MathsTest/gen/itt/t00154755/mathstest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package itt.t00154755.mathstest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- WirelessMouse/gen/com/t00154755/wirelessmouse/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.t00154755.wirelessmouse;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
